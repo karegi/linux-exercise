45e294f92d3b70bd0bb16404a9905fbe89a79e0c
karegi <mputhiamilka@gmail.com>
2024-02-28 17:11:27 +0300

    Add bioinformatics exercise files

---
+++ Exercise/scripts/extract_seq.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "grep  '>' /home/milka/Assignment1/linux-exercise/Exercise/data/sequences/*.fa | cut -d' ' -f1 > /home/milka/Assignment1/linux-exercise/Exercise/results/sequence_names.txt"
+echo "chmod +x /home/milka/Assignment1/linux-exercise/Exercise/scripts/extract_seq.sh"
